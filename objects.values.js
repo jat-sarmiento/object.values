c0c2f0021f2fa3cb3d5cc34f04d649a5f2849f2c
jat-sarmiento <67325915+jat-sarmiento@users.noreply.github.com>
2021-02-07 23:55:21 -0500

    Create objects.values.js

---
+++ objects.values.js
@@ -0,0 +1,8 @@
+        //object.values()
+ var perro = {
+  nombre: 'lulu',
+  edad: '42 meses',
+};
+console.log(Object.values(object1));
+/*nos muestra por consola este array en el cual nos muestro solo el valor de las propiedades del objeto
+ Array ['lulu', '42 meses']*/
